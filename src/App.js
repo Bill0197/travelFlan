6cda33ce8132be935d31c55c54db8f30e163dc55
Khabibullo Saydullaev <khabibullosaydullaev@gmail.com>
2021-08-20 21:41:17 +0500

    initalisez context

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
-import React, { useContext } from 'react'
+import React from 'react'
 import Home from './components/Home'
-import AuthContext from './store/auth-context'
 
 function App() {
-	const context = useContext(AuthContext)
 	return (
 		<React.Fragment>
-			<MainHeader />
-			<main>
-				{context.isLoggedIn && <Home />}
-			</main>
+			<Home />
 		</React.Fragment>
 	)
 }
