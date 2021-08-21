e0412c87d3ed33c15e87605df89f929a9b5df400
Khabibullo Saydullaev <khabibullosaydullaev@gmail.com>
2021-08-21 09:37:25 -0700

    adjusted title & internet explorer http-equiv

--- src/components/Login.js
+++ src/components/Login.js
@@ -26,11 +26,13 @@ const emailReducer = (state, action) => {
 
 const passwordReducer = (state, action) => {
 	if (action.type === 'USER_INPUT') {
-		return { value: action.val, isValid: action.val.trim().length > 6 }
+		return { value: action.val, isValid: action.val.trim().length > 7 }
 	}
+
 	if (action.type === 'INPUT_BLUR') {
-		return { value: state.value, isValid: state.value.trim().length > 6 }
+		return { value: state.value, isValid: state.value.trim().length > 7 }
 	}
+
 	return { value: '', isValid: false }
 }
 
